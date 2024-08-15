4f21b4589d296c06ed088248b21f4f71dd9fb629
Hanru Vorster <hanru.vorster@gmail.com>
2024-08-15 11:08:48 +0200

    Formatted comments and white space

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const prices = {
     "Tiramisu": 7.0,
     "Cheesecake": 6.5
 }
-let totalPrice = 0;
+let totalPrice = 0;                                         // set the starting price at R0.00 when the page loads and will be altered later
 
 // Function to display menu items by category
 function displayMenuItems(menu) { 
@@ -37,30 +37,25 @@ function displayMenuItems(menu) {
         });
     }         
 }
-displayMenuItems(menu)
+
 
 // Callback function for adding an item to the order
 function addToOrder(itemName) {
-    // Get the order items list and the order total element from the HTML
-    const orderContainer = document.getElementById('order');
-    const orderTotal = document.getElementById('order-total');
-    // Create a list item for the order
-    const orderList = document.createElement('li');
-    // Set the text content of the list item to the item name
-    orderList.textContent = itemName;
-    // Append the list item to the order items list
-    orderContainer.appendChild(orderList);
-    // Calculate and update the total price
-    const itemPrice = prices[itemName];
-    totalPrice += itemPrice;
-    // Update the text content of the order total element with the new total
-    orderTotal.textContent = totalPrice.toFixed(2);
+    const orderContainer = document.getElementById('order');    // Get the order items list and the order total element from the HTML
+    const orderTotal = document.getElementById('order-total');  // Get the order total element from HTML
+    const orderList = document.createElement('li');             // Create a list item for the order
+    
+    orderList.textContent = itemName;                           // Set the text content of the list item to the item name
+    orderContainer.appendChild(orderList);                      // Append the list item to the order items list
+    
+    const itemPrice = prices[itemName];                         // Calculate and update the total price
+    totalPrice += itemPrice;  
+    orderTotal.textContent = totalPrice.toFixed(2);             // Update the text content of the order total element with the new total
 }
 
 // Function to initialize the menu system
 function initMenuSystem(menu) {
-    // Call the function to display menu items
+    displayMenuItems(menu)                                      // Call the function to display menu items
 }
 
-// Start the menu system by calling the init function
-initMenuSystem(menu);
+initMenuSystem(menu);                                           // Start the menu system by calling the init function
